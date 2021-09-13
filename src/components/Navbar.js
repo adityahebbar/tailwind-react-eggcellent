5069fef5bf4f60ef186140edb4da5cc198855d21
adityahebbar <aditya_ps@outlook.com>
2021-09-13 11:59:58 +0530

    Added the Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => (
+  <nav className="flex items-center justify-between px-8 py-4 font-mono uppercase shadow">
+    <h3 className="text-xl font-bold">Egg</h3>
+    <div className="hidden md:block">
+      <Link to="/" className="p-4">
+        Home
+      </Link>
+      <Link to="/menu" className="p-4">
+        Menu
+      </Link>
+      <Link to="/about" className="p-4">
+        About
+      </Link>
+      <Link to="/contact" className="p-4">
+        Contact
+      </Link>
+    </div>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="w-6 h-6 md:hidden"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M4 6h16M4 12h16M4 18h16"
+      />
+    </svg>
+  </nav>
+);
+
+export default Navbar;
